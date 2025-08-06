44b16408a58cd2e1c94c02b7c828b4fd1a86abf6
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-08-06 20:36:42 -0300

    🐞 API-Express-MongoDB - tratando erros da rota autores

---
+++ curso-api-express-mongo-2/eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: { ...globals.node, ...globals.browser } } }, // Suporte a Node.js e Browser
+  pluginJs.configs.recommended,
+];
+
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+// ]);
